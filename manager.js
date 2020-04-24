51df68ad45b31d0c0776cc5502e39189a256b019
pioupiouz <sebastien.lin@epita.fr>
2020-04-24 16:00:53 +0200

    hello new pr

--- manager.js
+++ manager.js
@@ -1,5 +1,8 @@
 const MM = require('ms-manager');  
 let config = require(`./config.json`) || {};  
+const Sentry = require('@sentry/node');
+
+Sentry.init({ dsn: 'https://59600f70e9394101a8d5857429f3e746@o382925.ingest.sentry.io/5212518' });
 
 MM.init(config, (err, serviceInfo) => {  
     if (err) {  
